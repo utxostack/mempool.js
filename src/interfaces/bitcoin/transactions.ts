5f1be0f8bef984755de27da0fab3b73746a5d2ba
ahonn <yuexunjiang@gmail.com>
2024-08-06 16:35:34 +1000

    feat: add getTransactionTimes

--- src/interfaces/bitcoin/transactions.ts
+++ src/interfaces/bitcoin/transactions.ts
@@ -58,5 +58,6 @@ export interface TxInstance {
     vout: number;
   }) => Promise<TxOutspend>;
   getTxOutspends: (params: { txid: string }) => Promise<Array<TxOutspend>>;
+  getTransactionTimes: (params: { txId: string[] }) => Promise<Array<number>>;
   postTx: (params: { txhex: string }) => Promise<unknown>;
 }
